b2ee270ce039ac0533f19b4e5affd535f048cefb
mahmoud dayem <mahmoud.dayeem@gmail.com>
2025-07-29 00:13:04 +0300

    updated

--- screens/TaskDetailScreen.jsx
+++ screens/TaskDetailScreen.jsx
@@ -1,19 +1,20 @@
-import { StyleSheet, Text, View } from "react-native";
-import React, { useLayoutEffect, useState ,useEffect} from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useLayoutEffect, useState, useEffect } from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { loadData } from "../firebase/firebaseConfig";
 const TaskDetailScreen = () => {
   const route = useRoute();
   const navigation = useNavigation();
-  const [tasks,setTasks] = useState([]);
+  const [tasks, setTasks] = useState();
+
   useEffect(() => {
     const fetchCachedTasks = async () => {
       const cached = await loadData("cached_tasks");
       if (cached) {
         setTasks(cached); // set state with tasks
-        console.log('========= mahmoud abdeldayem= ccc ===============');
-        console.log(tasks[0].tasks);
-        console.log('====================================');
+        console.log("========= mohamed ===============");
+        console.log(tasks );
+        console.log("====================================");
       } else {
         console.log("No cached data found");
       }
@@ -22,7 +23,17 @@ const TaskDetailScreen = () => {
     fetchCachedTasks();
   }, []);
   // your helper
-
+  async function handlePress() {
+    const cached = await loadData("cached_tasks");
+    if (cached) {
+      setTasks(cached); // set state with tasks
+      console.log("========= Eyad Ammar Mazen===============");
+      console.log(tasks );
+      console.log("====================================");
+    } else {
+      console.log("No cached data found");
+    }
+  }
   const { task } = route.params;
   useLayoutEffect(() => {
     navigation.setOptions({ title: ` ${task.section}` });
@@ -30,6 +41,7 @@ const TaskDetailScreen = () => {
 
   return (
     <View style={styles.container}>
+      <Button title="update" onPress={handlePress} />
       <Text style={styles.title}>Task Details</Text>
       <Text style={styles.item}>Section: {task.section}</Text>
       <Text style={styles.item}>ID: {task.id}</Text>
