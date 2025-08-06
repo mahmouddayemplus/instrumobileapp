74337d624decf1c983b11399c39046fe3b604bf6
mahmouddayemplus <mahmoud.dayeem+@gmail.com>
2025-08-06 08:49:33 +0300

    config deleted

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -24,15 +24,7 @@ const firebaseConfig = {
   messagingSenderId: firebaseMessagingSenderId,
   appId: firebaseAppId,
 };
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAeYnIoSeivCEmRpclgvrim-MRQljti3ok",
-//   authDomain: "instruapp-6baab.firebaseapp.com",
-//   projectId: "instruapp-6baab",
-//   storageBucket: "instruapp-6baab.firebasestorage.app",
-//   messagingSenderId: "926409663138",
-//   appId: "1:926409663138:web:03170495c4b812ce8f2dfb"
-// };
-// Initialize Firebase
+ 
 const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 
