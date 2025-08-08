622d2816c652e10d6e1c0840d2c3d9b3c3b9fb64
mahmoud dayem <mahmoud.dayeem@gmail.com>
2025-08-08 20:12:46 +0300

    i built it and Ahmad Allah it is ok

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView, Alert } from 'react-native';
 //git remote set-url origin git@github.com:mahmouddayemplus/instrumobileapp.git
+//eas secret:push
 
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import PackerScreen from './screens/PackerScreen'
--- app.config.js
+++ app.config.js
@@ -1,46 +1,52 @@
-
 import 'dotenv/config'; // Load .env automatically
 
-export default{
-  "expo": {
-    "name": "instrupro",
-    
-    "slug": "instrupro",
-    "version": "1.0.0",
-     "extra": {
-      "firebaseApiKey": process.env.FIREBASE_API_KEY,
-      "firebaseAuthDomain": process.env.FIREBASE_AUTH_DOMAIN,
-      "firebaseProjectId": process.env.FIREBASE_PROJECT_ID,
-      "firebaseStorageBucket": process.env.FIREBASE_STORAGE_BUCKET,
-      "firebaseMessagingSenderId": process.env.FIREBASE_MESSAGING_SENDER_ID,
-      "firebaseAppId": process.env.FIREBASE_APP_ID
-    },
-    "orientation": "portrait",
-    "icon": "./assets/icon.png",
-    "userInterfaceStyle": "light",
-    "newArchEnabled": true,
-    "splash": {
-      "image": "./assets/splash-icon.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
-      
-    },
-    
-    "ios": {
-      "supportsTablet": true
-    },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/adaptive-icon.png",
-        "backgroundColor": "#ffffff"
+export default {
+  expo: {
+    name: "instrupro",
+    slug: "instrupro",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+
+    android: {
+      package: "com.mahmoud.instrupro", // ✅ required
+      versionCode: 1,
+      edgeToEdgeEnabled: true,
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
       },
-      "edgeToEdgeEnabled": true
     },
-    "web": {
-      "favicon": "./assets/favicon.png"
+
+    ios: {
+      buildNumber: "1.0.0",
+      supportsTablet: true,
     },
-    "plugins": [
-    "expo-sqlite"
-  ]
-  }
-}
+
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+
+    extra: {
+      eas: {
+        projectId: "1c920fd4-71ed-4340-8b70-8d83d7c42c98",
+      },
+      firebaseApiKey: process.env.FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.FIREBASE_APP_ID,
+    },
+
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+
+    plugins: ["expo-sqlite"],
+  },
+};
